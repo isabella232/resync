5dac0158b896eff12e751528d042c2bfdf5cb0e4
lindar <linda.reijnhoudt@dans.knaw.nl>
2015-08-31 14:42:15 +0200

    Format the testfiles according to PEP0008.
    E501 (lines should not exceed 79 chars) is ignored.

--- resync/test/test_client.py
+++ resync/test/test_client.py
@@ -2,13 +2,19 @@ import unittest
 import re
 import os
 import logging
-import sys, io, contextlib
+import sys
+import io
+import contextlib
 
 from resync.client import Client, ClientFatalError
 
-# From http://stackoverflow.com/questions/2654834/capturing-stdout-within-the-same-process-in-python
+# From
+# http://stackoverflow.com/questions/2654834/capturing-stdout-within-the-same-process-in-python
+
+
 class Data(object):
     pass
+
 
 @contextlib.contextmanager
 def capture_stdout():
@@ -30,96 +36,146 @@ class TestClient(unittest.TestCase):
     def test01_make_resource_list_empty(self):
         c = Client()
         # No mapping is an error
-        self.assertRaises( ClientFatalError, c.build_resource_list )
+        self.assertRaises(ClientFatalError, c.build_resource_list)
 
     def test02_bad_source_uri(self):
         c = Client()
-        self.assertRaises( ClientFatalError, c.baseline_or_audit )
-        c.set_mappings( ['http://example.org/bbb','/tmp/this_does_not_exist'] )
-        self.assertRaises( ClientFatalError, c.baseline_or_audit )
+        self.assertRaises(ClientFatalError, c.baseline_or_audit)
+        c.set_mappings(['http://example.org/bbb', '/tmp/this_does_not_exist'])
+        self.assertRaises(ClientFatalError, c.baseline_or_audit)
 
     def test06_write_capability_list(self):
         c = Client()
         with capture_stdout() as capturer:
-            c.write_capability_list( { 'a':'uri_a', 'b':'uri_b' } )
-        self.assertTrue( re.search(r'<urlset ',capturer.result) )
-        self.assertTrue( re.search(r'<rs:md capability="capabilitylist" />',capturer.result) )
-        self.assertTrue( re.search(r'<url><loc>uri_a</loc><rs:md capability="a"',capturer.result) )
-        self.assertTrue( re.search(r'<url><loc>uri_b</loc><rs:md capability="b"',capturer.result) )
+            c.write_capability_list({'a': 'uri_a', 'b': 'uri_b'})
+        self.assertTrue(re.search(r'<urlset ', capturer.result))
+        self.assertTrue(
+            re.search(r'<rs:md capability="capabilitylist" />',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>uri_a</loc><rs:md capability="a"',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>uri_b</loc><rs:md capability="b"',
+                      capturer.result))
 
     def test07_write_source_description(self):
         c = Client()
         with capture_stdout() as capturer:
-            c.write_source_description( [ 'a','b','c' ] )
-        #print capturer.result
-        self.assertTrue( re.search(r'<urlset ',capturer.result) )
-        self.assertTrue( re.search(r'<rs:md capability="description" />',capturer.result) )
-        self.assertTrue( re.search(r'<url><loc>a</loc><rs:md capability="capabilitylist" /></url>',capturer.result) )
-        self.assertTrue( re.search(r'<url><loc>b</loc><rs:md capability="capabilitylist" /></url>',capturer.result) )
+            c.write_source_description(['a', 'b', 'c'])
+        # print capturer.result
+        self.assertTrue(re.search(r'<urlset ', capturer.result))
+        self.assertTrue(
+            re.search(r'<rs:md capability="description" />', capturer.result))
+        self.assertTrue(re.search(
+            r'<url><loc>a</loc><rs:md capability="capabilitylist" /></url>',
+            capturer.result))
+        self.assertTrue(re.search(
+            r'<url><loc>b</loc><rs:md capability="capabilitylist" /></url>',
+            capturer.result))
+
+    def getUri(self, filename):
+        return 'file://' + \
+                os.path.abspath(
+                    'resync/test/testdata/examples_from_spec/'+filename)
 
     def test20_parse_document(self):
         # Key property of the parse_document() method is that it parses the
         # document and identifies its type
         c = Client()
         with capture_stdout() as capturer:
-            c.sitemap_name='file://' + os.path.abspath('resync/test/testdata/examples_from_spec/resourcesync_ex_1.xml')
+            c.sitemap_name = self.getUri('resourcesync_ex_1.xml')
             c.parse_document()
-        self.assertTrue( re.search(r'Parsed resourcelist document with 2 entries',capturer.result) )
+        self.assertTrue(
+            re.search(r'Parsed resourcelist document with 2 entries',
+                      capturer.result))
         with capture_stdout() as capturer:
-            c.sitemap_name='file://' + os.path.abspath('resync/test/testdata/examples_from_spec/resourcesync_ex_17.xml')
+            c.sitemap_name = self.getUri('resourcesync_ex_17.xml')
             c.parse_document()
-        self.assertTrue( re.search(r'Parsed resourcedump document with 3 entries',capturer.result) )
+        self.assertTrue(
+            re.search(r'Parsed resourcedump document with 3 entries',
+                      capturer.result))
         with capture_stdout() as capturer:
-            c.sitemap_name='file://' + os.path.abspath('resync/test/testdata/examples_from_spec/resourcesync_ex_19.xml')
+            c.sitemap_name = self.getUri('resourcesync_ex_19.xml')
             c.parse_document()
-        self.assertTrue( re.search(r'Parsed changelist document with 4 entries',capturer.result) )
+        self.assertTrue(
+            re.search(r'Parsed changelist document with 4 entries', capturer.result))
         with capture_stdout() as capturer:
-            c.sitemap_name='file://' + os.path.abspath('resync/test/testdata/examples_from_spec/resourcesync_ex_22.xml')
+            c.sitemap_name = self.getUri('resourcesync_ex_22.xml')
             c.parse_document()
-        self.assertTrue( re.search(r'Parsed changedump document with 3 entries',capturer.result) )
+        self.assertTrue(
+            re.search(r'Parsed changedump document with 3 entries',
+                      capturer.result))
 
     def test40_write_resource_list_mappings(self):
         c = Client()
-        c.set_mappings( ['http://example.org/','resync/test/testdata/'] )
+        c.set_mappings(['http://example.org/', 'resync/test/testdata/'])
         # with no explicit paths seting the mapping will be used
         with capture_stdout() as capturer:
             c.write_resource_list()
-        #sys.stderr.write(capturer.result)
-        self.assertTrue( re.search(r'<rs:md at="\S+" capability="resourcelist"', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.org/dir1/file_a</loc>', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.org/dir1/file_b</loc>', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.org/dir2/file_x</loc>', capturer.result ) )
+        # sys.stderr.write(capturer.result)
+        self.assertTrue(
+            re.search(r'<rs:md at="\S+" capability="resourcelist"',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.org/dir1/file_a</loc>',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.org/dir1/file_b</loc>',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.org/dir2/file_x</loc>',
+                      capturer.result))
 
     def test41_write_resource_list_path(self):
         c = Client()
-        c.set_mappings( ['http://example.org/','resync/test/testdata/'] )
-        # with an explicit paths setting only the specified paths will be included
+        c.set_mappings(['http://example.org/', 'resync/test/testdata/'])
+        # with an explicit paths setting only the specified paths will be
+        # included
         with capture_stdout() as capturer:
             c.write_resource_list(paths='resync/test/testdata/dir1')
         sys.stderr.write(capturer.result)
-        self.assertTrue( re.search(r'<rs:md at="\S+" capability="resourcelist"', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.org/dir1/file_a</loc>', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.org/dir1/file_b</loc>', capturer.result ) )
-        self.assertFalse( re.search(r'<url><loc>http://example.org/dir2/file_x</loc>', capturer.result ) )
-        # Travis CI does not preserve timestamps from github so test here for the file
-        # size but not the datestamp
+        self.assertTrue(
+            re.search(r'<rs:md at="\S+" capability="resourcelist"',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.org/dir1/file_a</loc>',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.org/dir1/file_b</loc>',
+                      capturer.result))
+        self.assertFalse(
+            re.search(r'<url><loc>http://example.org/dir2/file_x</loc>',
+                      capturer.result))
+        # Travis CI does not preserve timestamps from github so test here for
+        # the file size but not the datestamp
         #self.assertTrue( re.search(r'<url><loc>http://example.org/dir1/file_a</loc><lastmod>[\w\-:]+</lastmod><rs:md length="20" /></url>', capturer.result ) )
         #self.assertTrue( re.search(r'<url><loc>http://example.org/dir1/file_b</loc><lastmod>[\w\-:]+</lastmod><rs:md length="45" /></url>', capturer.result ) )
 
     def test50_update_resource_list(self):
-        c=Client()
-        c.set_mappings( ['http://example.org/','resync/test/testdata/'] )
+        c = Client()
+        c.set_mappings(['http://example.org/', 'resync/test/testdata/'])
         with capture_stdout() as capturer:
-            c.calculate_changelist(paths='resync/test/testdata/dir1', 
-                                   resource_sitemap='file://' + os.path.abspath('resync/test/testdata/examples_from_spec/resourcesync_ex_34.xml'), 
-                                   changelist_sitemap='file://' + os.path.abspath('resync/test/testdata/examples_from_spec/resourcesync_cl_01.xml'))
+            c.calculate_changelist(
+                paths='resync/test/testdata/dir1',
+                resource_sitemap=self.getUri('resourcesync_ex_34.xml'),
+                changelist_sitemap=self.getUri('resourcesync_cl_01.xml'))
         sys.stderr.write('----------\n')
         sys.stderr.write(capturer.result)
-        self.assertTrue( re.search(r'<rs:md capability="changelist"', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.org/dir1/file_a</loc>', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.org/dir1/file_b</loc>', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.com/res1.html</loc>', capturer.result ) )
-        self.assertTrue( re.search(r'<url><loc>http://example.com/res2.pdf</loc>', capturer.result ) )
+        self.assertTrue(
+            re.search(r'<rs:md capability="changelist"', capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.org/dir1/file_a</loc>',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.org/dir1/file_b</loc>',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.com/res1.html</loc>',
+                      capturer.result))
+        self.assertTrue(
+            re.search(r'<url><loc>http://example.com/res2.pdf</loc>',
+                      capturer.result))
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestClient)
--- resync/test/test_resource_list_multifile.py
+++ resync/test/test_resource_list_multifile.py
@@ -1,6 +1,5 @@
 import sys
 import unittest
-import io
 import tempfile
 import os.path
 import shutil
@@ -12,93 +11,104 @@ from resync.mapper import Mapper
 
 # etree gives ParseError in 2.7, ExpatError in 2.6
 etree_error_class = None
-if (sys.version_info < (2,7)):
+if (sys.version_info < (2, 7)):
     from xml.parsers.expat import ExpatError
     etree_error_class = ExpatError
 else:
     from xml.etree.ElementTree import ParseError
     etree_error_class = ParseError
 
+
 class TestResourceListMultifile(unittest.TestCase):
 
     def test_01_read_local_filenames(self):
         rl = ResourceList()
-        rl.read('file://' + os.path.abspath('resync/test/testdata/sitemapindex2/sitemap.xml'))
-        self.assertEqual( len(rl.resources), 17, '17 resources from 3 sitemaps listed')
+        rl.read(
+            'file://' + os.path.abspath('resync/test/testdata/sitemapindex2/sitemap.xml'))
+        self.assertEqual(len(rl.resources), 17,
+                         '17 resources from 3 sitemaps listed')
         sr = sorted(rl.uris())
-        self.assertEqual( sr[0], 'http://localhost:8888/resources/1' )
-        self.assertEqual( sr[1], 'http://localhost:8888/resources/10' )
-        self.assertEqual( sr[2], 'http://localhost:8888/resources/100' )
-        self.assertEqual( sr[3], 'http://localhost:8888/resources/1000' )
-        self.assertEqual( sr[16], 'http://localhost:8888/resources/826' )
+        self.assertEqual(sr[0], 'http://localhost:8888/resources/1')
+        self.assertEqual(sr[1], 'http://localhost:8888/resources/10')
+        self.assertEqual(sr[2], 'http://localhost:8888/resources/100')
+        self.assertEqual(sr[3], 'http://localhost:8888/resources/1000')
+        self.assertEqual(sr[16], 'http://localhost:8888/resources/826')
 
     def test_02_read_with_mapper(self):
         rl = ResourceList()
-        rl.mapper = Mapper(['http://localhost/='+'file://' + os.path.abspath('resync/test/testdata/sitemapindex2/')])
-        rl.read(uri='file://' + os.path.abspath('resync/test/testdata/sitemapindex2/sitemap_mapper.xml'))
-        self.assertEqual( len(rl.resources), 17, '17 resources from 3 sitemaps listed')
+        rl.mapper = Mapper(['http://localhost/=' + 'file://' +
+                            os.path.abspath('resync/test/testdata/sitemapindex2/')])
+        rl.read(uri='file://' +
+                os.path.abspath('resync/test/testdata/sitemapindex2/sitemap_mapper.xml'))
+        self.assertEqual(len(rl.resources), 17,
+                         '17 resources from 3 sitemaps listed')
         sr = sorted(rl.uris())
-        self.assertEqual( sr[0], 'http://localhost:8888/resources/1' )
-        self.assertEqual( sr[1], 'http://localhost:8888/resources/10' )
-        self.assertEqual( sr[2], 'http://localhost:8888/resources/100' )
-        self.assertEqual( sr[3], 'http://localhost:8888/resources/1000' )
-        self.assertEqual( sr[16], 'http://localhost:8888/resources/826' )
+        self.assertEqual(sr[0], 'http://localhost:8888/resources/1')
+        self.assertEqual(sr[1], 'http://localhost:8888/resources/10')
+        self.assertEqual(sr[2], 'http://localhost:8888/resources/100')
+        self.assertEqual(sr[3], 'http://localhost:8888/resources/1000')
+        self.assertEqual(sr[16], 'http://localhost:8888/resources/826')
 
     def test_11_write_multifile(self):
         tempdir = tempfile.mkdtemp(prefix='test_resource_list_multifile')
         rl = ResourceList()
         rl.mapper = Mapper(['http://localhost/=%s/' % (tempdir)])
-        rl.add( Resource( uri='http://localhost/a' ) )
-        rl.add( Resource( uri='http://localhost/b' ) )
-        rl.add( Resource( uri='http://localhost/c' ) )
-        rl.add( Resource( uri='http://localhost/d' ) )
+        rl.add(Resource(uri='http://localhost/a'))
+        rl.add(Resource(uri='http://localhost/b'))
+        rl.add(Resource(uri='http://localhost/c'))
+        rl.add(Resource(uri='http://localhost/d'))
         rl.max_sitemap_entries = 2
         # first try writing without mutlifile allowed
         rl.allow_multifile = False
-        self.assertRaises( ListBaseIndexError, rl.write, basename=os.path.join(tempdir,'sitemap.xml') )
+        self.assertRaises(ListBaseIndexError, rl.write,
+                          basename=os.path.join(tempdir, 'sitemap.xml'))
         # second actually do it
         rl.allow_multifile = True
-        rl.write( basename = os.path.join(tempdir,'sitemap.xml') )
+        rl.write(basename=os.path.join(tempdir, 'sitemap.xml'))
         # check the two component sitemaps
         rl1 = ResourceList()
-        rl1.read('file://' + os.path.abspath(os.path.join(tempdir,'sitemap00000.xml') ))
-        self.assertEqual( len(rl1), 2 )
-        self.assertEqual( rl1.capability, 'resourcelist' )
-        self.assertFalse( rl1.sitemapindex )
+        rl1.read(
+            'file://' + os.path.abspath(os.path.join(tempdir, 'sitemap00000.xml')))
+        self.assertEqual(len(rl1), 2)
+        self.assertEqual(rl1.capability, 'resourcelist')
+        self.assertFalse(rl1.sitemapindex)
         i = iter(rl1)
-        self.assertEqual( i.__next__().uri, 'http://localhost/a' )
-        self.assertEqual( i.__next__().uri, 'http://localhost/b' )
+        self.assertEqual(i.__next__().uri, 'http://localhost/a')
+        self.assertEqual(i.__next__().uri, 'http://localhost/b')
         rl2 = ResourceList()
-        #urllib.request.urlopen('file://' + os.path.abspath(os.path.join(tempdir,'sitemap00001.xml')))
-        rl2.read( 'file://' + os.path.abspath(os.path.join(tempdir,'sitemap00001.xml') ))
-        self.assertEqual( len(rl2), 2 )
+        # urllib.request.urlopen('file://' + os.path.abspath(os.path.join(tempdir,'sitemap00001.xml')))
+        rl2.read(
+            'file://' + os.path.abspath(os.path.join(tempdir, 'sitemap00001.xml')))
+        self.assertEqual(len(rl2), 2)
         i = iter(rl2)
-        self.assertEqual( i.__next__().uri, 'http://localhost/c' )
-        self.assertEqual( i.__next__().uri, 'http://localhost/d' )
+        self.assertEqual(i.__next__().uri, 'http://localhost/c')
+        self.assertEqual(i.__next__().uri, 'http://localhost/d')
         # check the sitemapindex (read just as index)
         rli = ResourceList()
-        rli.read('file://' + os.path.abspath( os.path.join(tempdir,'sitemap.xml')), index_only=True )
-        self.assertEqual( len(rli), 2 )
+        rli.read('file://' + os.path.abspath(os.path.join(tempdir,
+                                                          'sitemap.xml')), index_only=True)
+        self.assertEqual(len(rli), 2)
         i = iter(rli)
-        self.assertEqual( rli.capability, 'resourcelist' )
-        self.assertTrue( rli.sitemapindex )
-        self.assertEqual( i.__next__().uri, 'http://localhost/sitemap00000.xml' )
-        self.assertEqual( i.__next__().uri, 'http://localhost/sitemap00001.xml' )
+        self.assertEqual(rli.capability, 'resourcelist')
+        self.assertTrue(rli.sitemapindex)
+        self.assertEqual(i.__next__().uri, 'http://localhost/sitemap00000.xml')
+        self.assertEqual(i.__next__().uri, 'http://localhost/sitemap00001.xml')
         # check the sitemapindex and components
 #         rli = ResourceList( mapper=rl.mapper )
-        rli = ResourceList(mapper = Mapper(['http://localhost/='+'file://' + os.path.abspath(tempdir)]))
-        rli.read('file://' + os.path.abspath(os.path.join(tempdir,'sitemap.xml') ))
-        self.assertEqual( len(rli), 4 )
-        self.assertEqual( rli.capability, 'resourcelist' )
-        self.assertFalse( rli.sitemapindex )
+        rli = ResourceList(mapper=Mapper(
+            ['http://localhost/=' + 'file://' + os.path.abspath(tempdir)]))
+        rli.read('file://' + os.path.abspath(os.path.join(tempdir, 'sitemap.xml')))
+        self.assertEqual(len(rli), 4)
+        self.assertEqual(rli.capability, 'resourcelist')
+        self.assertFalse(rli.sitemapindex)
         i = iter(rli)
-        self.assertEqual( i.__next__().uri, 'http://localhost/a' )
-        self.assertEqual( i.__next__().uri, 'http://localhost/b' )
-        self.assertEqual( i.__next__().uri, 'http://localhost/c' )
-        self.assertEqual( i.__next__().uri, 'http://localhost/d' )
+        self.assertEqual(i.__next__().uri, 'http://localhost/a')
+        self.assertEqual(i.__next__().uri, 'http://localhost/b')
+        self.assertEqual(i.__next__().uri, 'http://localhost/c')
+        self.assertEqual(i.__next__().uri, 'http://localhost/d')
         # cleanup tempdir
         shutil.rmtree(tempdir)
 
-if  __name__ == '__main__':
+if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestResourceListMultifile)
     unittest.TextTestRunner(verbosity=2).run(suite)
