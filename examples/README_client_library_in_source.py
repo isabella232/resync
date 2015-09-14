3bd71adba2411d8a26d9ffafbedc6fe232583856
henk van den berg <ecco@Hor-Vastus.local>
2015-09-14 13:27:58 +0200

    last print statements without ()

--- examples/README_client_library_in_source.py
+++ examples/README_client_library_in_source.py
@@ -6,4 +6,4 @@ if (True): #keep indentation of README
     rl = ResourceList()
     rl.add( Resource('http://example.com/res1', lastmod='2013-01-01') )
     rl.add( Resource('http://example.com/res2', lastmod='2013-01-02') )
-    print rl.as_xml()
+    print(rl.as_xml())
